c686cea75345a6d9c295ebebf5964e4eff6778fd
Hugo <hugorayodam@gmail.com>
2026-05-06 17:55:40 +0200

    corregir la prueba1

--- Pytest Reto 5/proyecto/src/prueba1.py
+++ Pytest Reto 5/proyecto/src/prueba1.py
@@ -2,11 +2,11 @@ import pymysql
 
 def conexion():
     conn = pymysql.connect(
-    host='127.0.0.1',
-    port=3306,
-    user='root',
-    password='Segura123',
-    database='Gastro_basque',
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='Segura123',
+        database='Gastro_basque',
     )
     return conn
 
@@ -18,7 +18,9 @@ def recoge_nombre(usuario):
     cursor = conn.cursor()
     usuario_comprobar = usuario
     sql_state_nombre = "SELECT password_hash from usuario where nombre = %s"
-    cursor.execute(sql_state_nombre, (usuario_comprobar))
+    cursor.execute(sql_state_nombre, (usuario_comprobar,))
     usuario_bd_ps = cursor.fetchone()
     cerrar_conexion(conn)
+    if usuario_bd_ps is None:
+        return None
     return usuario_bd_ps[0]
