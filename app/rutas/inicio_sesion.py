eada65be420acebf595cfbe3aa957e80ce400a96
Filip <filipiworowicz@gmail.com>
2026-05-07 13:00:46 +0200

    Implementado el get y post para el inicio de sesion

--- app/rutas/inicio_sesion.py
+++ app/rutas/inicio_sesion.py
@@ -3,19 +3,22 @@ from modelos.base_datos import recoge_nombre
 
 inicio_bp = Blueprint('sesion', __name__)
 
-@inicio_bp.route('/sesion', methods=['POST'])
+@inicio_bp.route('/sesion', methods=['GET','POST'])
+
 def check_sesion():
-
-    print("ENTRANDO EN CHECK_SESION")
-    usuario = request.form['usuario']
-    contra = request.form['contraseña']
-
-    usuario_check = recoge_nombre(usuario)
-    print(f"La contraseña llega? {usuario_check} y lo que le paso es {contra}")
-    if contra == usuario_check:
-        print("Sesion iniciada")
-        session['usuario'] = usuario
-        return render_template('index.html')
-    else:
-        print("Contraseña o usuario incorrecto")
+    if request.method == 'GET':
         return render_template('prueba.html')
+    else:
+        print("ENTRANDO EN CHECK_SESION")
+        usuario = request.form['usuario']
+        contra = request.form['contraseña']
+
+        usuario_check = recoge_nombre(usuario)
+        print(f"La contraseña llega? {usuario_check} y lo que le paso es {contra}")
+        if contra == usuario_check:
+            print("Sesion iniciada")
+            session['usuario'] = usuario
+            return render_template('index.html')
+        else:
+            print("Contraseña o usuario incorrecto")
+            return render_template('prueba.html')
