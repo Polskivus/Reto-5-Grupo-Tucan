fda925532917ac1927b845236d0545822e7a8ab7
Filip <filipiworowicz@gmail.com>
2026-05-07 21:08:13 +0200

    Comentadas las lineas que aun no estan en uso en la aplicacion

--- app/main.py
+++ app/main.py
@@ -7,16 +7,16 @@ def create_app():
     app.config.from_object(Config)
 
     # Registrar blueprints (rutas)
-    from rutas.recetas import recetas_bp
-    from rutas.dietas  import dietas_bp
+#   from rutas.recetas import recetas_bp
+#    from rutas.dietas  import dietas_bp
+#    from rutas.nominas import nominas_bp
     from rutas.index import main_bp
-    from rutas.nominas import nominas_bp
     from rutas.inicio_sesion import inicio_bp
-    app.register_blueprint(inicio_bp)
-    app.register_blueprint(nominas_bp)
     app.register_blueprint(main_bp)
-    app.register_blueprint(recetas_bp)
-    app.register_blueprint(dietas_bp)
+    app.register_blueprint(inicio_bp)
+#    app.register_blueprint(nominas_bp)
+#    app.register_blueprint(recetas_bp)
+#    app.register_blueprint(dietas_bp)
 
     return app
 
